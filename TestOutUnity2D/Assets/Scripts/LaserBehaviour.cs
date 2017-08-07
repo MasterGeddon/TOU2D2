44421ff477074c7e583d701b8becfe0ec1cd89d6
MasterGeddon <gregor.ober@infoteq>
2017-08-07 09:14:55 +0200

    laser itse a laser

---
+++ TestOutUnity2D/Assets/Scripts/LaserBehaviour.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dd52cf22ed74e99ca3d14d0ffd4bcc1d61d5f6f69d165e6de7791fd6fcfdd5d6
+size 427
--- TestOutUnity2D/Assets/Scripts/PlayerBehaviour.cs
+++ TestOutUnity2D/Assets/Scripts/PlayerBehaviour.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fd8bf1ca75966aa506cad25872c1a7b473b1e904efb61af5915cb8133c74dcd
-size 1428
+oid sha256:fadd0485fccb2c61c9a728961a7b03369e531795262089e44d3a7ee2cd76eba2
+size 2458
